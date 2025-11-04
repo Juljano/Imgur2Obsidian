0fa7ca9a1fb70bc2dc6a7e9c51ae827daf739f17
jml <jml@jml.de>
2025-11-04 11:30:03 +0100

    - Imgur replaced with Postimages - Added Selenium & Beautifulsoup

--- main.py
+++ main.py
@@ -1,73 +1,139 @@
 import tkinter as tk
 from tkinter import filedialog as fd
+from selenium import webdriver
 import requests
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
-##Useless
-client_id = "anonym"
-url = "https://api.imgur.com/3/image"
 
 
 class ImgurToObsidian:
     def __init__(self):
         self.root = tk.Tk()
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        self.driver = webdriver.Chrome()
 
-        self.choose_button = tk.Button()
-        self.upload_button = tk.Button()
-        self.entry_field = tk.Entry()
-        self.open_window()
+        self.entry_field = None
+        self.starting_webdriver()
+        self.setup_gui()
+
+    def setup_gui(self):
+        self.root.geometry("600x600")
+        self.root.resizable(False, False)
+        self.root.title("Postimages-Uploader")
+
+        message = tk.Label(self.root, text="Please choose an image to upload to Postimages.", font=("", 14, "bold"))
+        message.pack()
+
+        choose_button = tk.Button(self.root, text="Choose an image & Upload", command=self.select_file, width=30, height=2)
+        choose_button.pack(pady=30)
+
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
+        self.root.mainloop()
+
+    def on_close(self):
+        self.driver.quit()
+        self.root.destroy()
 
     def select_file(self):
         filetypes = (
-            ('Imgur supported image files',
-             ('*.jpg', '*.jpeg', '*.png', '*.gif')),
+            ('Imgur supported image files', ('*.jpg', '*.jpeg', '*.png', '*.gif')),
             ('JPEG-Bilder', '*.jpg'),
             ('PNG-Bilder (auch animiert APNG)', ('*.png', '*.apng')),
-            ('GIF-Bilder', '*.gif'))
+            ('GIF-Bilder', '*.gif')
+        )
 
         image_path = fd.askopenfilename(
-            title='Open a image',
+            title='Open an image',
             initialdir='/',
-            filetypes=filetypes)
+            filetypes=filetypes
+        )
         if image_path:
             self.upload_image(image_path)
 
-    def open_window(self):
-        self.root.geometry("600x600")
-        self.root.resizable(False, False)
-        self.root.title("Imgur-Uploader")
+    def starting_webdriver(self):
+        try:
+            self.driver.get("https://postimages.org/login")
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div/div/div[1]/form[1]/div[1]/div[2]/input"))
+            )
+            self.login()
+        except Exception as error:
+            print(f"Webdriver error: {error}")
 
-        message = tk.Label(self.root, text="Please choose an image to upload to Imgur.", font=("", 14, "bold"))
-        message.pack()
+    def login(self):
+        try:
+            username_field = self.driver.find_element(By.XPATH, "/html/body/div/div/div/div/div[1]/form[1]/div[1]/div[2]/input")
+            username_field.send_keys("moellerjoy@outlook.com")
 
-        self.choose_button = tk.Button(self.root, text="Choose an image & Upload", command=self.select_file, width=30,height=2)
-        self.choose_button.pack(side="top", anchor="n", pady=30)
+            password_field = self.driver.find_element(By.XPATH, "/html/body/div/div/div/div/div[1]/form[1]/div[2]/div[2]/input")
+            password_field.send_keys("fG9tFyC6S2g6")
 
-        self.root.mainloop()
+            login_button = self.driver.find_element(By.XPATH, "//*[@id='content']/div/div/div[1]/form[1]/div[3]/div/input")
+            login_button.click()
+
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[@id='content']/div/div[1]/div/h1")))
+        except Exception as error:
+            print(f"Webdriver error: {error}")
+
+
 
     def upload_image(self, image_path):
         try:
-            headers = {
-                "Authorization": f"Client-ID {client_id}"
+            cookies = self.driver.get_cookies()
+            session_cookies = {cookie['name']: cookie['value'] for cookie in cookies}
+
+            payload = {
+                "gallery": "2j5R5rk",  # Obsidian Album-id
+                "optsize": "0",
+                "expire": "0",
+                "numfiles": "1",
+                "upload_session": "1762247250951.019885489023204306"
             }
+            headers = {
+                "User-Agent": "Mozilla/5.0",
+                "X-Requested-With": "XMLHttpRequest"
+            }
+
             with open(image_path, "rb") as image_file:
-                payload = {
-                    "image": image_file
+                files = {
+                    "file": ("Images.jpg", image_file, "image/jpeg")
                 }
-                response = requests.post(url, headers=headers, files=payload)
+                response = requests.post("https://postimages.org/json/rr", data=payload, files=files, headers=headers, cookies=session_cookies)
 
             if response.status_code == 200:
-                entry_field = tk.Entry(self.root, bd=2, width=70)
-                entry_field.insert(0, f"![{"Name"}]({response.json()['data']['link']})")
-                entry_field.pack(pady=10)
-
+                image_link = response.json()['url']
+                self.get_images_format(image_link)
             else:
-                response = tk.Label(self.root, text=f"Fehler beim Upload: {response.status_code}: {response.text}")
-                response.pack(expand=True)
+                error_label = tk.Label(self.root, text=f"Fehler beim Upload: {response.status_code}: {response.text}")
+                error_label.pack(expand=True)
         except requests.exceptions.RequestException as error:
-            response = tk.Label(self.root, text=f"Fehler beim Upload: {error}")
-            response.pack(expand=True)
+            error_label = tk.Label(self.root, text=f"Fehler beim Upload: {error}")
+            error_label.pack(expand=True)
+
+    def get_images_format(self, image_link):
+        try:
+            response = requests.get(image_link)
+            html = response.text
+            soup = BeautifulSoup(html, 'html.parser')
+            image_tag = soup.find('img', {'id': 'main-image'})
+            if image_tag:
+                image_url = image_tag['src']
+
+                self.entry_field = tk.Entry(self.root, bd=2, width=70)
+                self.entry_field.insert(0, f"![Title]({image_url})")
+                self.entry_field.pack(pady=10)
+            else:
+                print("Kein Bild mit ID 'main-image' gefunden.")
+        except IOError as error:
+            print(error)
 
 
 if __name__ == '__main__':
-    imgur = ImgurToObsidian()
-    imgur.open_window()
+    ImgurToObsidian()
